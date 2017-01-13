b8d1cb94acfff568f2d6baab123a1e504f381219
webrx <webrx@126.com>
2017-01-13 11:11:19 +0800

    Initial commit

---
+++ src/com/fz/Test.java
@@ -0,0 +1,10 @@
+package com.fz;
+
+/**
+ * Created by webrx on 2017/1/13 0013 11:10.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world.....");
+    }
+}
